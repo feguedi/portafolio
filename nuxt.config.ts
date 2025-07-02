38c661230b7d7f31bfbf4dc2923c35bed320b834
Ixe Fernández <feguedi@yahoo.com.mx>
2025-07-02 16:37:32 -0500

    Modificada configuración de Nuxt
    - Agregados assets de estilos e imágenes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
+import path from 'node:path';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: 'Ixe Fernández | Desarrollador web',
+
+      meta: [
+        { charset: 'UTF-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      ],
+    },
+  },
+
+  alias: {
+    '@': path.resolve(__dirname),
+  },
+
+  css: [
+    '@/assets/styles.css',
+  ],
 
   modules: [
     '@nuxt/fonts',
@@ -10,4 +31,4 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/scripts'
   ]
-})
+});
